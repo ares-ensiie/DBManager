f014272adc92c62a7cf6cb8ab6908367d9fff10b
Jonathan Hurter <jonathan@hurter.fr>
2016-02-12 20:29:35 +0100

    Ajout de la création d'une BDD postgres

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -1,4 +1,6 @@
 require 'securerandom'
+require 'pg'
+
 
 class DatabasesController < ApplicationController
   before_action :set_database, only: [:show, :edit, :update, :destroy]
@@ -23,10 +25,20 @@ class DatabasesController < ApplicationController
   # POST /databases.json
   def create
     @database = Database.new(database_params)
+    @database.name = @database.name.downcase
     @database.user = @user["uid"]
     @database.password = SecureRandom.urlsafe_base64(16)
+    result = true
+    if @database.postgres?
+      result = create_postgresql_db(@database.name, @database.password)
+    end
+
+    if result then
+      result = @database.save
+    end
+
     respond_to do |format|
-      if @database.save
+      if result
         format.html { redirect_to @database, notice: 'La base de données à bien été créée.' }
         format.json { render :show, status: :created, location: @database }
       else
@@ -55,5 +67,52 @@ class DatabasesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def database_params
       params.require(:database).permit(:name, :type)
+    end
+
+    def create_postgresql_db(db, password)
+      puts "************************"
+      puts "* POSTGRES DB CREATION *"
+      puts "************************"
+      conn = PG.connect(host: SERVERS_INFOS["postgres"]["ip"], 
+                        port: SERVERS_INFOS["postgres"]["port"], 
+                        user: SERVERS_INFOS["postgres"]["user"], 
+                        password: SERVERS_INFOS["postgres"]["password"], 
+                        dbname: "postgres")
+      
+      puts "DB already exists ?"
+      # On test si la DB existe déjà
+      res = conn.exec_params("SELECT COUNT(*) from pg_database WHERE datname=$1",[db]);
+
+      if res.getvalue(0,0) != "0" then
+        puts "y"
+        return false
+      end
+      puts "n"
+
+      puts "Role already exists ?"
+      # On test si l'utilisateur existe déjà
+      res = conn.exec_params("SELECT COUNT(*) FROM pg_roles WHERE rolname=$1",[db]);
+      if res.getvalue(0,0) != "0" then
+        puts "y"
+        return false
+      end
+      puts "n"
+
+
+      # On essaie de créer la BDD et l'utilisateur
+      begin
+        puts "Creating user #{db} : #{password}"
+        conn.exec("CREATE ROLE #{db} WITH LOGIN PASSWORD '#{password}'")
+        puts "Creating db #{db}"
+        conn.exec("CREATE DATABASE #{db}")
+        puts "Grant"
+        conn.exec("GRANT ALL PRIVILEGES ON DATABASE #{db} TO #{db}")
+      rescue Exception => e
+        puts e
+        puts "Failed !"
+        return false
+      end
+      puts "Done !"
+      return true
     end
 end
